55059eabb3ebef26e0812751338c63509d9441dd
connormoffatt <connormoffatt4@gmail.com>
2020-06-15 08:51:10 -0700

    clean up syntax
    review initial code

--- mOnEyBaLl.py
+++ mOnEyBaLl.py
@@ -7,188 +7,188 @@ Created on Thu Jun 11 14:04:26 2020
 import scipy.stats as sts
 import numpy as np
 
+
 def margins_from_ratings(ratings):
     return
-    
-    
+
 
 def success_prob_from_meta_margin(meta_margin, meta_sigma=0.03, meta_df=2):
-    '''Given the meta-margin for the party reaching the desired threshold and 
-    the parameters determining the t-distribution about this margin, returns the 
-    party's probability of reaching this threshold.
+    '''Given the meta-margin for the party reaching the desired threshold and
+    the parameters determining the t-distribution about this margin, returns
+    the party's probability of reaching this threshold.
 
     Arguments:
         meta_margin: real number, as calculated by find_meta_margin
-        
-        meta_sigma: positive real number, estimate of standard deviation of 
+
+        meta_sigma: positive real number, estimate of standard deviation of
             actual meta-margin
-        
+
         race_df: positive integer, degrees of freedom used in t-distribution
-    
-    Output: real number between 0 and 1, probability of party reaching 
+
+    Output: real number between 0 and 1, probability of party reaching
             threshold
     '''
-    
-    return sts.t.cdf(meta_margin/meta_sigma, meta_df)
+    return sts.t.cdf(meta_margin / meta_sigma, meta_df)
 
 
 def success_prob_from_margin(margin, race_sigma=0.03, race_df=2):
     ''' Given the expected margin of a race and the parameters determining the
     t-distribution about this margin, returns the probability of victory.
-    
+
     Arguments:
-        margin: real number between -1 and 1, expected win margin for candidate 
+        margin: real number between -1 and 1, expected win margin for candidate
             (negative = loss margin)
-        
-        race_sigma: positive real number, estimate of standard deviation of 
+
+        race_sigma: positive real number, estimate of standard deviation of
             actual win margin
-        
+
         race_df: positive integer, degrees of freedom used in t-distribution
-    
+
     Output: real number between 0 and 1, probability of candidate winning
     '''
+    return sts.t.cdf(margin / race_sigma, race_df)
 
-    return sts.t.cdf(margin/race_sigma, race_df)
-    
 
 def chamber_success_prob_assuming_independence(margins, threshold):
     ''' Given a list of expected win margins and the number of seats needed
     for the party to have desired redistricting power, find the probability
-    of the party reaching that threshold. Relies on success_prob_from_margin 
+    of the party reaching that threshold. Relies on success_prob_from_margin
     and assumes independence of races.
-    
+
     Arguments:
-        margins: numpy array of expected win margins (between -1 and 1) for 
+        margins: numpy array of expected win margins (between -1 and 1) for
             party (negative = loss margin)
-        
+
         threshold: number of seats needed for redistricting power
-        
+
     Output: probability that the party reaches the threshold number of seats,
             assuming independence of race outcomes
     '''
-    
+
     # find probability of victory for each race
     probs = [success_prob_from_margin(i) for i in margins]
-    
-    ## Find full probability distribution of seats won, assuming independence ##
-    
+
+    # # Find full probability distribution of seats won, assuming independence
+
     # associate with each race a polynomial of the form lose_prob + win_prob*x
-    polys = [np.asarray([1-p, p]) for p in probs]
-    
+    polys = [np.asarray([1 - p, p]) for p in probs]
+
     # multiply all of these polynomials; the resulting coefficient of x^n
-    # is the nth element of the list (index starting at 0), and is the 
+    # is the nth element of the list (index starting at 0), and is the
     # probability of winning exactly n seats
     seat_probs = np.asarray([1])
     for poly in polys:
         seat_probs = np.polymul(seat_probs, poly)
-        
+
     # return the probability of reaching the threshold
     return np.sum(seat_probs[threshold:])
 
-   
+
 def find_meta_margin(margins, seats_needed):
     ''' Given a list of expected win margins and the number of seats needed
     for the party to have desired redistricting power, finds the Meta-Margin.
-    
-    The Meta-Margin is the margin that all seats would have to shift uniformly 
-    so that the probability of the party reaching that threshold is 
-    exactly 0.50, assuming independence of races. (By convention, 
+
+    The Meta-Margin is the margin that all seats would have to shift uniformly
+    so that the probability of the party reaching that threshold is
+    exactly 0.50, assuming independence of races. (By convention,
     positive Meta-Margin implies the party is favored.)
-    
-    The Meta-Margin isunique because the win probability varies monotonically 
-    with the uniform shift. The idea here is to use bisection to estimate the 
-    Meta-Margin numerically. 
-    
+
+    The Meta-Margin is unique because the win probability varies monotonically
+    with the uniform shift. The idea here is to use bisection to estimate the
+    Meta-Margin numerically.
+
     Arguments:
-        margins: numpy array of expected win margins for party 
+        margins: numpy array of expected win margins for party
             (negative = loss margin)
-            
+
         seats_needed: number of seats needed for redistricting power
-    
-    Output: Meta-Margin 
-    ''' 
+
+    Output: Meta-Margin
+    '''
 
     # set bounds for Meta-Margin, initialize accuracy threshold eps
     min_margin = -30
     max_margin = 30
     eps = 1e-15
-    
+
     # while we still have a range of uncertainty of length at least eps
     while max_margin - min_margin > eps:
-        
+
         # find the middle of the range, shift all margins by this amount
         # against the party
-        average = (min_margin + max_margin)/2
+        average = (min_margin + max_margin) / 2
         adjusted_margins = margins - average
-        
+
         # find the win probability with these shifted margins
-        prob = chamber_success_prob_assuming_independence\
-                    (adjusted_margins, seats_needed)
-                    
-        # depending on whether this probability is below or above 0.5, 
+        prob = chamber_success_prob_assuming_independence(adjusted_margins,
+                                                          seats_needed)
+
+        # depending on whether this probability is below or above 0.5,
         # modify the search bounds appropriately
         if prob < 0.5:
             max_margin = average
         else:
             min_margin = average
-    
+
     # return an endpoint of the interval, which has converged to a point
     return min_margin
- 
+
+
 def chamber_success_prob(margins, threshold):
     ''' Given a list of expected win margins and the number of seats needed
     for the party to have desired redistricting power, find the probability
     of the party reaching that threshold. Uses Meta-Margin technique.
-    
+
     Arguments:
-        margins: numpy array of expected win margins (between -1 and 1) for 
+        margins: numpy array of expected win margins (between -1 and 1) for
             party (negative = loss margin)
-        
+
         threshold: number of seats needed for redistricting power
     '''
-    
+
     return success_prob_from_meta_margin(find_meta_margin(margins, threshold))
 
+
 def voter_power(districts_df, seats_needed):
-    ''' Finds the power of one vote in each district (i.e. the increase in 
-    probability that the party reaches the necessary number of seats if they gain
-    one extra vote) 
-    
+    ''' Finds the power of one vote in each district (i.e. the increase in
+    probability that the party reaches the necessary number of seats if they
+    gain one extra vote)
+
     Arguments:
         districts_df: pandas DataFrame of districts, indexed by 0, 1, 2, ..
             with (at least) these columns:
-            'MARGIN': the expected winning margin for the party (negative if 
+            'MARGIN': the expected winning margin for the party (negative if
                     losing margin)
             'NUM_VOTERS': the number of voters in the district
-            
-        seats_needed: number of seats needed for redistricting power 
-        
+
+        seats_needed: number of seats needed for redistricting power
+
     Output: input DataFrame with one column added, 'VOTER_POWER', which
             gives the result of the calculation for each district'''
-            
+
     # for all races in districts_df
     for ix, race in districts_df.iterrows():
-        
+
         # grab all margins, deep copy in numpy format
         margins = districts_df['MARGIN']
         margins = np.asarray([i for i in margins])
-        
-        # find the chamber success probability 
+
+        # find the chamber success probability
         prob = chamber_success_prob(margins, seats_needed)
-        
+
         # grab the number of voters in the district of interest
         num_voters = race['NUM_VOTERS']
-        
+
         # adjust the margin in that race, assuming the party gained 1 vote
-        margins[ix] = margins[ix] + 1/num_voters
-        
-        ## NOTE: 1 may be too small, with the effect so small it might get into 
-        ## floating point error. Need to check. Maybe boost to 10 or 100.
-        
-        # find the chamber success probability 
+        margins[ix] = margins[ix] + 1 / num_voters
+
+        # NOTE: 1 may be too small, with the effect so small it might get into
+        # floating point error. Need to check. Maybe boost to 10 or 100.
+
+        # find the chamber success probability
         prob_new = chamber_success_prob(margins, seats_needed)
-        
+
         # calcuate vote power and add to proper row of districts_df
         districts_df.loc[ix, 'VOTER_POWER'] = prob_new - prob
-        
+
     return districts_df
--- president_analysis.py
+++ president_analysis.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """
 Created on Thu May 28 15:29:43 2020
 
@@ -6,211 +5,192 @@ Created on Thu May 28 15:29:43 2020
 """
 import pandas as pd
 import numpy as np
-from datetime import date, timedelta, datetime
-from dateutil.parser import parse
-import scipy.stats as st
 from scipy.stats import norm
-import matplotlib.pyplot as plt
-from sklearn.calibration import calibration_curve
+from datetime import datetime as dt
+from datetime import timedelta
 
-# download EV file
-ev = pd.read_csv("C:\\Users\\Jacob\\Documents\\GitHub\\elections\\ev.csv")
-ev.set_index('State', inplace=True)
 
-# download file to dataframe
-df = pd.read_csv\
-        ("C:\\Users\\Jacob\\Documents\\GitHub\\elections\\raw-polls.csv") 
-        
-# get rid of this year, national polls
-df = df[df['year'] != 2020]
-df = df[df['location'] != 'US']
+def within_14(election_day, poll_day):
+    """Check if poll day is within two weeks of election day"""
+    return election_day - timedelta(days=14) < poll_day
 
-# put 'polldate' column in datetime date format
-def format_date(i):
-    return parse(i).date()
-df['polldate'] = df['polldate'].apply(format_date)
-    
-# restrict to specific type of race
-race_type = 'Pres-G'
-df = df[df['type_simple'] == race_type]
 
-# get all races polled, at all levels where they were polled
-# for example, the 2004 presidential race in FL is different from the
-# 2004 presidential race in MI
-races = list(set(df['race']))
-
-# initialize output df
-out_df = pd.DataFrame({'race': races})
-out_df['polls_used'] = ''
-out_df['num_polls_used'] =''
-out_df['median_winning_margin'] = ''
-out_df['median_absolute_deviation'] = ''
-out_df['SEM'] = ''
-out_df['win_probability'] = ''
-out_df['actual_winning_margin'] = ''
-out_df['error_of_median'] = ''
-out_df['won_race'] = ''
-out_df['year'] = ''
-out_df['state'] = ''
-out_df.set_index('race', inplace=True)
-
-# set year and state field
-for i, _ in out_df.iterrows():
-    out_df.loc[i, 'year'] = int(i[0:4])
-    out_df.loc[i, 'state'] = i[-2:]
-
-# Determine which polls to use according to the following rules:
-# 1. Consider only the most recent poll for a given pollster
-#
-# 2. Use all polls taken in the two weeks prior to election day, or use the
-# last three polls, whichever gives more data
-
-# for each race
-for race in races:
-    
-    # splice DataFrame to only consider polls from this race
-    race_df = df[df['race'] == race]
-    
-    # find the year
-    year = list(race_df['year'])[0]
-    
-    # find general election day on this year, the Tuesday in the range 11/2-11/8 
-    for day in [2, 3, 4, 5, 6, 7, 8]:
-        if date(year, 11, day).weekday() == 1:
-            election_day = date(year, 11, day)
-    
-
-    ## Filter out earlier polls by same pollster
-    
-    pollsters = list(set(race_df['pollster_rating_id']))
-    
-    # for each pollster
-    for pollster in pollsters:
-        
-        # find the last date among their polls
-        pollster_df = race_df[race_df['pollster_rating_id'] == pollster]
-        last_date = sorted(pollster_df['polldate'])[-1]
-        
-        # delete all polls from this pollster that do not occur on this date
-        race_df = race_df[(race_df['pollster_rating_id'] != pollster) | \
-                          (race_df['polldate'] == last_date)]
-    
-    # Find all polls in the 14 days before election day
-    def time_to_election(i):
-        return (election_day - i).days
-    last_14_df = race_df[race_df['polldate'].apply(time_to_election) <= 14]
-    
-    # if there are at least 3 such polls
-    if len(last_14_df) >= 3:
-        # use these polls
-        out_df.loc[race]['polls_used'] = last_14_df
-        
-    # if there are less than 3 such polls
-    else:
-        # sort the polls earliest to latest
-        race_df = race_df.sort_values(by='polldate')
-        # use the last 3 polls
-        out_df.loc[race]['polls_used'] = race_df[-3:]
-        
-## Fill in the remaining columns of out_df based on the polls used
-
-# number of polls used
-out_df['num_polls_used'] = out_df['polls_used'].apply(len)
- 
 # median winning margin in polls of candidate 1
 def median_win_margin(polls_df):
+    """Extract the median margin."""
     return np.median(np.asarray(polls_df['cand1_pct'] - polls_df['cand2_pct']))
-out_df['median_winning_margin'] = out_df['polls_used'].apply(median_win_margin)
 
-# median absolute deviation from median 
+
 def median_deviation(polls_df):
+    """Calculate median absolute deviation (MAD)."""
     margins = np.asarray(polls_df['cand1_pct'] - polls_df['cand2_pct'])
     median = np.median(margins)
     deviations = np.abs(margins - median)
     return np.median(deviations)
-out_df['median_absolute_deviation'] = \
-            out_df['polls_used'].apply(median_deviation)
-            
 
-# this formula is per Sam's documentation, where he says we calculate the 
-# standard error of the mean, but with the median in place of the mean
-# and (median absolute deviation from median)/0.675 in place of the stanard
-# deviation
+
 def SEM(polls_df):
+    """Standard error of the mean.
+
+    To prevent errors from outliers. Use th emedian in place of the mean and
+    the MAD / 0.675 in place of the standard deviation.
+    """
     med_deviation = median_deviation(polls_df)
-    
-    
     stddev = med_deviation / 0.675
-    
     return stddev / np.sqrt(len(polls_df))
-out_df['SEM'] = out_df['polls_used'].apply(SEM)
 
-# win probability of candidate 1
-# we find the probability that a Gaussian random variable with 
-# distruibuted according to ~ N(median, SEM) is greater than 0.    
-def win_probability(polls_df):
+
+def win_prob(polls_df):
+    """Assume normal dist and calculute probability candidate 1 wins."""
     median = median_win_margin(polls_df)
-    sem = SEM(polls_df) 
-    # the absurd case where SEM = 0
-    if sem == 0:
-        sem = 0.0001
-        
-    # 2020 modification, require SEM >= 3
-    sem = 3
-            
-    return np.round(1000*norm.cdf(median/sem))/1000
-out_df['win_probability'] = \
-            out_df['polls_used'].apply(win_probability)
-    
-# actual win margin
+    sem = SEM(polls_df)
+
+    # Require SEM >= 3
+    sem = np.maximum(sem, 3)
+
+    # Get the probability from a normal distribution
+    return np.round(1000 * norm.cdf(median / sem)) / 1000
+
+
 def actual_win_margin(polls_df):
-    return max(polls_df['cand1_actual']) - max(polls_df['cand2_actual'])  
-out_df['actual_winning_margin'] = out_df['polls_used'].apply(actual_win_margin)
-
-# error of the polling median
-out_df['error_of_median'] = \
-    out_df['median_winning_margin'] - out_df['actual_winning_margin'] 
-# won race
-def won_race(margin):
-    return margin > 0 
-out_df['won_race'] = out_df['actual_winning_margin'].apply(won_race)
-
-# drop polls used
-out_df = out_df.drop(columns=['polls_used'])
+    """Use max since all actual results are equal to get result margin."""
+    return max(polls_df['cand1_actual']) - max(polls_df['cand2_actual'])
 
 
-### SOME TESTING (TRASH) ###
-def win_prob_2(race):
-    m = race['median_winning_margin']
-    n = race['num_polls_used']
-    return st.t.cdf(m*(n+3)/(2*n+26), 10)
-for i, race in out_df.iterrows():
-    out_df.loc[i, 'win_probability'] = win_prob_2(race)
+def main():
+    """Perform presidential voter power analysis."""
+    # import electoral votes and polls
+    ev = pd.read_csv('ev.csv').set_index('State')
+    df = pd.read_csv('raw-polls.csv')
 
-    
-def calibrate():
-    probs = []
-    actuals = []
-    for percent in np.linspace(0, 1, 101):
-        idxs = [i for i, x in out_df.iterrows() if out_df.loc[i, 'win_probability'] <= percent+0.1 and out_df.loc[i, 'win_probability'] >= percent-0.1]
-        probs.append(np.mean([out_df.loc[i, 'win_probability'] for i in idxs]))
-        actuals.append(sum([out_df.loc[i, 'won_race'] for i in idxs])/len([out_df.loc[i, 'won_race'] for i in idxs]))
-    return probs, actuals
+    # Only use state polls before 2020
+    df = df[(df['year'] < 2020) & (df['location'] != 'US')]
 
-# calculates the average polling error in a year, weighted roughly by population
-def weighted_national_error(year):
-    # set year for electoral votes
-    if year > 2010:
-        ev_year = '2010'
-    else:
-        ev_year = '2000'
-        
-    year_df = out_df[out_df['year'] == year]
-    year_df = year_df[year_df['state'].isin(list(ev.index))]
-    errors = [year_df.loc[race, 'error_of_median'] for race, _ in year_df.iterrows()]
-    evs = [ev.loc[i['state'], ev_year] for race, i in year_df.iterrows()]
-    
-    # find average
-    return sum(np.asarray(errors) * (np.asarray(evs) - 2)) / np.sum(np.asarray(evs) - 2)
+    # Make polldate attribute a datetime
+    df['polldate'] = df['polldate'].apply(lambda x: dt.strptime(x, '%m/%d/%Y'))
 
-out_df['adjusted_error_of_median'] = out_df['error_of_median'] - out_df['year'].apply(weighted_national_error)
+    # Only use president general polls
+    df = df[df['type_simple'] == 'Pres-G']
+
+    # Create dataframe for each relevant race attribute. For example the 04
+    # FL pres. race will have a different label than the 04 MI pres. race
+    races = list(set(df['race']))
+    out_cols = ['race', 'polls_used', 'num_polls', 'median_win_marg', 'MAD',
+                'SEM', 'win_prob', 'actual_win_marg', 'error_of_median',
+                'won_race', 'year', 'state']
+    out_df = pd.DataFrame({'race': races}, columns=out_cols).fillna('')
+
+    # Set year and state
+    out_df['year'] = out_df['race'].apply(lambda x: x[0:4])
+    out_df['state'] = out_df['race'].apply(lambda x: x[-2:])
+
+    # Set index to race
+    out_df = out_df.set_index('race')
+
+    # Determine which polls to use according to the following rules:
+    # 1. Consider only the most recent poll for a given pollster
+    #
+    # 2. Use all polls taken in the two weeks prior to election day, or use the
+    # last three polls, whichever gives more data
+
+    for race in races:
+        # only consider polls from the current race
+        race_df = df[df['race'] == race]
+
+        # Get the year of the race
+        year = list(race_df['year'])[0]
+
+        # Extract general election day
+        for day in [2, 3, 4, 5, 6, 7, 8]:
+            if dt(year, 11, day).weekday() == 1:
+                elec_day = dt(year, 11, day)
+                break
+
+        # Filter out earlier polls
+        for pollster in race_df['pollster_rating_id'].unique():
+            # find the last date of the pollster
+            pollster_df = race_df[race_df['pollster_rating_id'] == pollster]
+            last_date = sorted(pollster_df['polldate'])[-1]
+
+            # Delete all polls from this pollster before this date
+            race_df = race_df[(race_df['pollster_rating_id'] != pollster) |
+                              (race_df['polldate'] == last_date)]
+
+            # Get all polls 14 days before election day
+            last_14_df = race_df[race_df['polldate'].apply(lambda x:
+                                                           within_14(elec_day,
+                                                                     x))]
+
+            # Save polls used in the output dataframe
+            # if there are at least 3 polls keep the last_14_df
+            if len(last_14_df) >= 3:
+                out_df.loc[race]['polls_used'] = last_14_df
+            # if there are less than 3 polls within 14 days keep last 3 polls
+            else:
+                race_df = race_df.sort_values(by='polldate')
+                out_df.loc[race]['polls_used'] = race_df[-3:]
+
+    # The remaining attributes in out_df will be filled using the polls
+    # we saved in the polls_used attribute
+
+    # Get the (1) number of polls, (2) median winning margin, (3) mean absolute
+    # deviation, (4) standard error of the mean, but with the median and
+    # median absolute deviation
+    out_df['num_polls'] = out_df['polls_used'].apply(len)
+    out_df['median_win_marg'] = out_df['polls_used'].apply(median_win_margin)
+    out_df['MAD'] = out_df['polls_used'].apply(median_deviation)
+    out_df['SEM'] = out_df['polls_used'].apply(SEM)
+
+    # Calculate the win probability of candidate 1
+    out_df['win_prob'] = out_df['polls_used'].apply(win_prob)
+
+    # Get the actual winning margin
+    out_df['actual_win_marg'] = out_df['polls_used'].apply(actual_win_margin)
+
+    # Get the error of the polling median
+    out_df['error_of_median'] = out_df['median_win_marg']
+    out_df['error_of_median'] -= out_df['actual_win_marg']
+
+    # Boolean if candidate 1 won the race
+    out_df['won_race'] = (out_df['actual_win_marg'] > 0)
+
+    # Apply buckets for calibration
+    bins = np.linspace(-0.01, 1, 102)
+    out_df['bucket_ix'] = pd.cut(out_df['win_prob'], bins, labels=False)
+
+    # Remove the polls used
+    out_df = out_df.drop(columns=['polls_used'])
+
+    # Calculate the weighted national error and join. This will calculate the
+    # average polling error in a year and is weighted by electoral votes - 2
+    nat_err_df = pd.DataFrame()
+    for year in out_df['year'].unique():
+        # Get the correct year for electoral votes
+        if int(year) > 2010:
+            ev_year = '2010'
+        else:
+            ev_year = '2000'
+
+        # Get the out_df for the specific year
+        year_df = out_df[out_df['year'] == year]
+        year_df = year_df[year_df['state'].isin(list(ev.index))]
+        errors = [year_df.loc[race, 'error_of_median']
+                  for race, _ in year_df.iterrows()]
+        evs = [ev.loc[i['state'], ev_year] for race, i in year_df.iterrows()]
+
+        # Get average error using population estimate
+        pop_est = np.array(evs) - 2
+        avg_error = np.dot(np.array(errors), pop_est) / np.sum(pop_est)
+        nat_err_df.at[year, 'wt_national_error'] = avg_error
+
+    # Join to add weighted national error to out_df
+    out_df = out_df.merge(nat_err_df, left_on='year', right_index=True)
+
+    # Calculate the adjusted error of the median
+    out_df['adj_error_of_median'] = out_df['error_of_median']
+    out_df['adj_error_of_median'] -= out_df['wt_national_error']
+    return out_df
+
+
+if __name__ == "__main__":
+    main()
