3f05353b9061d8e6c64eed12a7d9fe3451bddf05
connormoffatt <connormoffatt4@gmail.com>
2020-07-08 19:37:54 -0700

    Add geopandas helper functions

---
+++ geo_helper/create_shapefiles.py
@@ -0,0 +1,29 @@
+import helper.helper_tools.shp_manipulation as sm
+import helper.helper_tools.file_management as fm
+
+
+def dissolve_by_attribute(in_path, dissolve_attribute, out_path=False):
+	'''Remove boundaries according to attribute.
+
+	Dissolve boundaries for shapefile(s) according to a given attribute. We
+	will also check for contiguity after boundaries have been dissolved.
+
+	Arguments:
+		in_path:
+			full path to input shapefile to be dissolved
+
+		out_path:
+			full path to save created shapefile
+
+		disolve_attribute:
+			attribute to dissolve boundaries by
+	'''
+	#  Generate dissolved shapefile
+	df = fm.load_shapefile(in_path)
+	df = sm.dissolve(df, dissolve_attribute)
+
+	# Save shapefile
+	if out_path:
+		fm.save_shapefile(df, out_path)
+
+	return df
