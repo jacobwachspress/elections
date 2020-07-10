a617be8ac10c1ed2cadcfa9449bf6f35daa820b6
jacobwachspress <jacobmw@princeton.edu>
2020-07-10 16:44:07 -0400

    imputation

--- analyze_cnalysis_2018.py
+++ analyze_cnalysis_2018.py
@@ -49,14 +49,17 @@ for state, _ in sts_df.iterrows():
         
         # find count of races in this category
         sts_df.loc[state, conf + '_count'] = len(df)
+        
+        # find count of races in this category
+        sts_df.loc[state, conf + '_correct'] = len(df[df['correct'] == True])
                                         
         # find mean winning margin
         sts_df.loc[state, conf + '_mean_win_margin'] = \
                                         df['actual_win_margin'].mean()
         
-        # find stdev of winning margin
-        sts_df.loc[state, conf + '_stdev_win_margin'] = \
-                                df['actual_win_margin'].std()
+        # find variance of winning margin
+        sts_df.loc[state, conf + '_variance_win_margin'] = \
+                                df['actual_win_margin'].var()
         
         # if there are races
         if len(df) > 0:                 
@@ -67,5 +70,68 @@ for state, _ in sts_df.iterrows():
             sts_df.loc[state, conf + '_t_mean'] = mean
             sts_df.loc[state, conf + '_t_sigma'] = sigma
             sts_df.loc[state, conf + '_t_deg_f'] = deg_f
+            
+    conf = 'ALL'
+    df = state_df.copy()
+    
+    # find all outliers (probably surprise uncontesteds)
+    outliers_df = df[df['win_margin'] > 0.9]
+    sts_df.loc[state, conf + '_was_uncontested'] = len(outliers_df)
+    
+    # find all outlier misses
+    outlier_losers_df = outliers_df[outliers_df['correct'] == False]
+    sts_df.loc[state, conf + '_lost_uncontested'] = len(outlier_losers_df)
+    
+    # delete outliers
+    df = df[df['win_margin'] <= 0.9]
+    
+    # find count of races in this category
+    sts_df.loc[state, conf + '_count'] = len(df)
+    
+    # find count of races in this category
+    sts_df.loc[state, conf + '_correct'] = len(df[df['correct'] == True])
+                                    
+    # find mean winning margin
+    sts_df.loc[state, conf + '_mean_win_margin'] = \
+                                    df['actual_win_margin'].mean()
+    
+    # find variance of winning margin
+    sts_df.loc[state, conf + '_variance_win_margin'] = \
+                            df['actual_win_margin'].var()
         
+    
+    # build dictionary of expected win margin by confidence
+    expected_win_margins = {}
+    for conf in confidences:
+        expected_win_margins[conf] = sts_df.loc['USA', conf + '_mean_win_margin']
         
+    # add expected win margin column to results DataFrame
+    results['expected_win_margin'] = results['confidence'].apply(lambda x: \
+           expected_win_margins[x])
+    
+    # add R overperformance column
+    results['R_overperformance'] = results.apply(lambda x: \
+        x['actual_win_margin']-x['expected_win_margin'] if x['predicted_winner'] == 'R'\
+        else x['expected_win_margin']-x['actual_win_margin'] if x['predicted_winner'] == 'D'\
+        else 0, axis=1)
+    
+    # find average R_overperformance by state, add column to isolate race effects
+    
+    # remove safe seats (uncontested issues)
+    no_safe = results[results['confidence'] != 'Safe']
+    
+    # remove predicted I winners
+    no_safe = no_safe[no_safe['predicted_winner'] != 'I']
+    
+    # dictionary of mean R_overperformance
+    R_dict = no_safe.groupby(['state_po'])['R_overperformance'].mean().to_dict()
+    
+    # add column with these numbers
+    results['state_R_overperformance'] = results['state_po'].apply(lambda x: \
+                                                   R_dict[x])
+    
+    # add column with isolated error, in direction of GOP
+    results['isolated_race_error'] = results.apply(lambda x: \
+           x['R_overperformance']-x['state_R_overperformance'], axis=1)
+    
+    no_safe = results[results['confidence'] != 'Safe']
--- clean_fundamentals.py
+++ clean_fundamentals.py
@@ -35,7 +35,22 @@ def main():
                     'fundamentals\\raw\\pres_results_by_state_leg_district\\'
     df_st_leg = get_all_st_leg_pres_results(input_path, df_fips)
     df_st_leg.to_csv(path + 'clean/st_leg_pres_results.csv', index=False)
-
+    
+    # Get partisan residual of each st leg district
+    df_st_leg_pr = get_st_leg_dist_partisan_residual(df_st_leg, df_state)
+    df_st_leg_pr.to_csv(path + 'clean/st_leg_partisan_residual.csv',
+                    index=False)
+    
+    # impute residuals where there is no data
+    sldu_labels = pd.read_csv(path + 'clean/sldu_labels.csv', dtype=str)
+    sldl_labels = pd.read_csv(path + 'clean/sldl_labels.csv', dtype=str)
+    st_leg_res = pd.read_csv(path + 'clean/st_leg_partisan_residual.csv', \
+                                         dtype=str)
+    cong_res = pd.read_csv(path + 'clean/cong_dist_partisan_residual.csv', \
+                                         dtype=str)
+    up, low = impute_residuals(sldu_labels, sldl_labels, st_leg_res, cong_res)
+    up.to_csv(path + 'clean/imputed_sldu_residuals.csv')
+    low.to_csv(path + 'clean/imputed_sldl_residuals.csv')
 
 def get_statewide_presidential_results(df):
     """Calculate 2-party voteshare in 2012 and 2016 for president by state.
@@ -308,6 +323,171 @@ def get_cong_dist_partisan_residual(df, df_state):
     # Reduce to relevant columns and save
     df = df[['state', 'geoid', 'district_num', 'resid']]
     return df
+
+def get_st_leg_dist_partisan_residual(df, df_state):
+    """Calculate partisan residual for each st_leg district.
+
+    Partisan residual is defined as follows:
+
+        Average of congressional district presidential vote in 2012 and 2016
+
+        MINUS
+
+        Average of statewide district presidential vote in 2012 and 2016
+
+    Note that if we don't have 2012 data for a congressional district, we
+    simply impute the 2016 result
+
+    Arguments:
+        df: cleaned congressional district presidential results
+
+        df_state: cleaned statewide presidential results
+    """
+    # Impute 2012 voteshare with 2016 and vice versa
+    df['dem_12'] = df['dem_12'].fillna(df['dem_16'])
+    df['dem_16'] = df['dem_16'].fillna(df['dem_12'])
+
+    # Calculate average dem voteshare
+    df['cd_dem'] = (df['dem_12'] + df['dem_16']) / 2
+    df_state['state_dem'] = (df_state['dem_12'] + df_state['dem_16']) / 2
+
+    # Reduce state to relevant columns
+    df_state = df_state[['state', 'state_dem']]
+
+    # Join state value to congressional districts
+    df = df.merge(df_state)
+
+    # Calculate congressional district residual
+    df['resid'] = df['cd_dem'] - df['state_dem']
+
+    # Reduce to relevant columns and save
+    df = df[['state', 'office', 'geoid', 'district_num', 'resid']]
+    return df
+
+def impute_residuals(sldu_labels, sldl_labels, st_leg_res, cong_res):
+    ''' Give partisan residual of every state legislative district, making the
+    best guess where there is no data.
+    
+    No data for lower -> check upper seat with largest overlap
+    No data for upper -> check congress seat with largest overlap
+    
+    Arguments:
+        sldu_labels: all upper chamber districts with geoid and cd_geoid needed
+            for imputation
+        sldl_labels: all lower chamber districts with geoid and sldu_geoid + 
+            cd_geoid needed for imputation
+        st_leg_res: partisan residuals of st_leg districts
+        cong_res: partisan residuals of congressional districts
+    
+    Output:
+        sldu_labels, sldl_labels with a few columns added
+            - partisan residual
+            - imputed?
+            - imputed_from what level
+    '''
+    # deep copy labels dataframes just to avoid pandas warning when we modify
+    sldu_labels = sldu_labels.copy()
+    sldl_labels = sldl_labels.copy()
+    
+    # fill in upper chamber resid's
+    
+    # restrict residuals table to upper chamber
+    upper_res = st_leg_res[st_leg_res['office'] == 'upper']
+    
+    # for each sldu district
+    for i, row in sldu_labels.iterrows():
+        
+        # lookup district's geoid in residuals table 
+        geoid = row['geoid']
+        match_df = upper_res[upper_res['geoid'] == geoid]
+        
+        # if there are multiple matches, we have a data problem
+        assert len(match_df) < 2, "duplicate geo_ids in upper residuals dataframe @" \
+                + geoid
+        
+        # if we have a match
+        if len(match_df) == 1:
+            
+            # update residual and other relevant fields in sldu_labels
+            sldu_labels.loc[i, 'resid'] = list(match_df['resid'])[0]
+            sldu_labels.loc[i, 'imputed'] = False
+            sldu_labels.loc[i, 'imputed_from'] = 'upper'
+        
+        # if no match, impute residual from congressional level
+        else:
+            
+            # get geoid of congressional district to impute from
+            cong_geoid = row['cd_geoid']
+            
+            # lookup geoid in cong_dist residuals table 
+            match_df = cong_res[cong_res['geoid'] == cong_geoid]
+            
+            # if there are multiple matches, we have a data problem
+            assert len(match_df) < 2, "duplicate geo_ids in cong residuals dataframe @" \
+                + cong_geoid
+            
+            # if we have a match
+            if len(match_df) == 1:
+                
+                # update residual and other relevant fields in sldu_labels
+                sldu_labels.loc[i, 'resid'] = list(match_df['resid'])[0]
+                sldu_labels.loc[i, 'imputed'] = True
+                sldu_labels.loc[i, 'imputed_from'] = 'congress'
+            
+    # fill in lower chamber resid's
+    
+    # restrict residuals table to lower chamber
+    lower_res = st_leg_res[st_leg_res['office'] == 'lower']
+    
+    # for each sldl district
+    for i, row in sldl_labels.iterrows():
+        
+        # lookup district's geoid in residuals table 
+        geoid = row['geoid']
+        match_df = lower_res[lower_res['geoid'] == geoid]
+        
+        # if there are multiple matches, we have a data problem
+        assert len(match_df) < 2, "duplicate geo_ids in lower residuals dataframe @" \
+                + geoid
+        
+        # if we have a match
+        if len(match_df) == 1:
+            
+            # update residual and other relevant fields in sldu_labels
+            sldl_labels.loc[i, 'resid'] = list(match_df['resid'])[0]
+            sldl_labels.loc[i, 'imputed'] = False
+            sldl_labels.loc[i, 'imputed_from'] = 'lower'
+            
+        # if no match, impute residual from congressional level
+        else:
+            
+            # get geoid of sldu district to impute from
+            sldu_geoid = row['sldu_geoid']
+            
+            # lookup geoid in cong_dist residuals table 
+            match_df = sldu_labels[sldu_labels['geoid'] == sldu_geoid]
+            
+            # if there are multiple matches, we have a data problem
+            assert len(match_df) < 2, "duplicate geo_ids in upper residuals dataframe"
+            # if there is no match, we have a data problem
+            assert len(match_df) > 0, "bad sldu_geoid matching @ " + sldu_geoid
+            
+                
+            # update residual and other relevant fields in sldu_labels
+            sldl_labels.loc[i, 'resid'] = list(match_df['resid'])[0]
+            sldl_labels.loc[i, 'imputed'] = True
+            sldl_labels.loc[i, 'imputed_from'] = \
+                                        list(match_df['imputed_from'])[0]
+    
+    return sldu_labels, sldl_labels
+                
+            
+            
+            
+            
+        
+        
+        
 
 
 if __name__ == "__main__":
