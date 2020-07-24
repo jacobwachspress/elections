4d080da0716bbd439c4fc285a041bbfa8ccd5334
connormoffatt <connormoffatt4@gmail.com>
2020-07-23 21:59:16 -0700

    Add clean foundations predictions for 2020

--- foundations_prediction_2020.py
+++ foundations_prediction_2020.py
@@ -11,102 +11,95 @@ def main():
     df_state = pd.read_csv(path + 'clean/state_pres_results.csv')
     economist_path = money_path + 'elections/'
     economist_path += 'economist_projected_margins_most_recent.csv'
-    df_econ = pd.read_csv(economist_path)
+    df_economist = pd.read_csv(economist_path)
 
     # Get cleaned moneyball data
     df_lower = pd.read_csv(money_path + 'state/lower_input_data.csv')
     df_upper = pd.read_csv(money_path + 'state/upper_input_data.csv')
 
-    # Load election results
-    df_elec_18 = pd.read_csv(path + 'clean/election_results_2018.csv')
+    # Format district number and drop unnecessary columns
+    df_lower = clean_incumbency(df_lower)
+    df_upper = clean_incumbency(df_upper)
+    d_num = 'district_num'
+    df_lower[d_num] = df_lower[d_num].astype(str).str.zfill(3)
+    df_upper[d_num] = df_upper[d_num].astype(str).str.zfill(3)
+    keep_cols = ['state', 'district_num', 'incumbent_18', '2016_win_margin',
+                 '2016_win_party']
+    df_lower = df_lower[keep_cols]
+    df_upper = df_upper[keep_cols]
 
-    # Convert 2018 results to dem voteshare
-    df_16 = pd.read_csv(money_path + 'state/upper_chamber_results_2016.csv')
-    df_elec_lower, df_elec_upper = results_to_dem_voteshare(df_elec_18, df_16)
-    df_elec_lower.to_csv(path + 'temp/lower_voteshare.csv', index=False)
-    df_elec_upper.to_csv(path + 'temp/upper_voteshare.csv', index=False)
+    # Load election results and incumbency results
+    df_elec = pd.read_csv(money_path + 'elections/election_results_2018.csv')
+    df_inc_lower = pd.read_csv(path +
+                               'clean/lower_chamber_incumbency_2016_2020.csv')
+    df_inc_upper = pd.read_csv(path +
+                               'clean/upper_chamber_incumbency_2016_2020.csv')
 
-    # Get difference between 2018 election and foundation prediction
+    # Load district based residuals
     df_lower_resid = pd.read_csv(path + 'clean/imputed_sldl_residuals.csv')
     df_upper_resid = pd.read_csv(path + 'clean/imputed_sldu_residuals.csv')
-    df_diff_lower = foundation_diff(df_state, df_lower_resid, df_elec_lower)
-    df_diff_upper = foundation_diff(df_state, df_upper_resid, df_elec_upper)
-    df_diff_lower.to_csv(path + 'temp/fund_diff_lower.csv', index=False)
-    df_diff_upper.to_csv(path + 'temp/fund_diff_upper.csv', index=False)
 
-    # Get foundation prediction
-    df_proj_lower = foundation_prediction(df_lower, df_econ, df_diff_lower)
-    df_proj_upper = foundation_prediction(df_upper, df_econ, df_diff_upper)
-    df_proj_lower.to_csv(path + 'clean/proj_lower_2020.csv', index=False)
-    df_proj_upper.to_csv(path + 'clean/proj_upper_2020.csv', index=False)
+    # Compile historical results
+    df = compile_historical_results(df_elec, df_lower, df_upper, df_inc_lower,
+                                    df_inc_upper)
 
+    # Add 2016 election results
+    df = add_national_results(df, df_state, df_lower_resid, df_upper_resid)
+
+    # Calculate incumbency advantage
+    df = incumbency_advantage(df)
+
+    # Calculate foundations prediction
+    df = foundation_prediction(df, df_economist)
+    df.to_csv(path + 'clean/foundations_predictions_2020.csv', index=False)
     return
 
 
-def results_to_dem_voteshare(df, df_16):
-    """Convert 2018 election results to dem voteshare.
-
-    We assume that independents would caucus with the opposite party or else
-    they would not be running.
-        Note: if this is not true, we need to incorporate a way to change
-              impute that the election is uncontested
-
-    If a candidate gets more than 80% of the 3 party voteshare, we assume it is
-    uncontested or essentially uncontested because the oppositing candidate is
-    not representative of actual competition
-
-    Impute independent votes for losing party if losing party did not have a
-    candidate
+def compile_historical_results(df, df_lower, df_upper, df_inc_lower,
+                               df_inc_upper):
+    """Compile 2018 and 2016 election results.
 
     Arguments:
-        df: 2018 election results.
+        df: 2018 election data
 
-        df_16: 2016 senate election results for KS and MN. They elect all
-               state senators every 4 years
+        df_lower: lower chamber input data
 
-    Output:
-        df_lower: cleaned dataframe for lower chambers
+        df_upper: upper chamber input data
 
-        df_upper: cleaned dataframe for upper chambers
+        df_inc_lower: 2016 and 2020 incumbency data
+
+        df_inc_upper: 2016 and 2020 incumbendy data
     """
-    # Reduce vote count names
+    # Get district number as zfilled to 3
+    df['district_num'] = df['district_num'].astype(str).str.zfill(3)
+    d_num = 'district_num'
+    df_inc_lower[d_num] = df_inc_lower[d_num].astype(str).str.zfill(3)
+    df_inc_upper[d_num] = df_inc_upper[d_num].astype(str).str.zfill(3)
+
+    # Simplify vote count names
     df['D'] = df['democrat']
     df['R'] = df['republican']
     df['I'] = df['independent']
-    df = df.drop(columns=['democrat', 'republican', 'independent', 'year',
-                          'dem_two', 'rep_two', 'district'])
+    drop_cols = ['democrat', 'republican', 'independent', 'year', 'dem_two',
+                 'rep_two', 'district', 'dem_cand', 'ind_cand', 'rep_cand']
+    df = df.drop(columns=drop_cols)
 
-    # Add 2016 upper chambers in MN and KS
-    df_16['chamber'] = 'upper'
-    df_16['D'] = df_16['dem']
-    df_16['R'] = df_16['rep']
-    df_16['I'] = df_16['other']
-    df_16['three_sum'] = df_16['D'] + df_16['R'] + df_16['I']
-    df_16['dem_three'] = df_16['D'] / df_16['three_sum']
-    df_16['ind_three'] = df_16['I'] / df_16['three_sum']
-    df_16['rep_three'] = df_16['R'] / df_16['three_sum']
-    df_16 = df_16.drop(columns=['dem', 'rep', 'other', 'three_sum'])
-    df_16['district_num'] = df_16['district_num'].astype(str).str.zfill(3)
-    df = df.append(df_16)
-
-    # Fix Alaska tie
+    # Fix the tie in Alaska
     df.loc[(df['state'] == 'AK') & (df['district_num'] == '001'), 'R'] += 1
 
-    # Get winning party (fix AK tie)
+    # Get the winning party
     df['win_party'] = df[['D', 'R', 'I']].idxmax(axis=1)
 
-    # Get whether it was essentially uncontested
+    # Check if the race was essentially uncontested
     df['max_three'] = df[['dem_three', 'rep_three', 'ind_three']].max(axis=1)
     df['uncontested'] = df['max_three'] >= 0.75
-    df = df.drop('max_three', axis=1)
+    drop_cols = ['max_three', 'dem_three', 'ind_three', 'rep_three']
+    df = df.drop(columns=drop_cols)
 
-    # Get the party that got the least amount of votes
+    # Get the party that got the least votes
     df['worst_party'] = df[['D', 'R', 'I']].idxmin(axis=1)
 
-    # Add flag if independent was in top two
-    df['I_top_two'] = df['worst_party'] != 'I'
-
-    # Impute independent as opposite party if opposite party had no candidate
+    # Impute independent as other party if other party has no candidate
     df['D'] = df.apply(lambda r: r['I'] if ((r['win_party'] == 'R') &
                                             (r['D'] == 0)) else r['D'], axis=1)
     df['R'] = df.apply(lambda r: r['I'] if ((r['win_party'] == 'D') &
@@ -116,115 +109,173 @@ def results_to_dem_voteshare(df, df_16):
     df['dem_elec'] = df['D'] / (df['R'] + df['D'])
 
     # Split into lower and upper
-    df_lower = df[df['chamber'] == 'lower']
-    df_upper = df[df['chamber'] == 'upper']
+    df_elec_lower = df[df['chamber'] == 'lower']
+    df_elec_upper = df[df['chamber'] == 'upper']
 
-    # Keep relevant columns
-    keep_cols = ['state', 'district_num', 'win_party', 'I_top_two',
-                 'uncontested', 'dem_elec']
-    df_lower = df_lower[keep_cols]
-    df_upper = df_upper[keep_cols]
-    return df_lower, df_upper
+    # Drop relevant columns
+    drop_cols = ['D', 'R', 'I', 'worst_party', 'chamber']
+    df_elec_lower = df_elec_lower.drop(columns=drop_cols)
+    df_elec_upper = df_elec_upper.drop(columns=drop_cols)
 
+    # Rename columns
+    new_cols = ['state', 'district_num', 'win_party_18', 'uncontested_18',
+                'dem_share_18']
+    df_elec_lower.columns = new_cols
+    df_elec_upper.columns = new_cols
 
-def foundation_diff(df_state, df, df_elec):
-    """Difference between election outcome and 2018 foundation prediction.
+    # Join to input data
+    df_lower = df_lower.merge(df_elec_lower, on=['state', 'district_num'],
+                              how='left')
+    df_upper = df_upper.merge(df_elec_upper, on=['state', 'district_num'],
+                              how='left')
 
-    Arguments:
-        df_state: 2016 state presidential election results
+    # Clean data if incumbent won election in 2016
+    df_inc_lower['incumbent_16'] = df_inc_lower['incumbent']
+    df_inc_upper['incumbent_16'] = df_inc_upper['incumbent']
+    keep_cols = ['state', 'district_num', 'incumbent_16']
+    df_inc_lower = df_inc_lower[keep_cols]
+    df_inc_upper = df_inc_upper[keep_cols]
 
-        df: lower/upper chamber presidential election residuals
+    # Join incumbency data
+    df_lower = df_lower.merge(df_inc_lower, on=['state', 'district_num'],
+                              how='left')
+    df_upper = df_upper.merge(df_inc_upper, on=['state', 'district_num'],
+                              how='left')
 
-        df_elec: lower/upper chamber dem voteshare results in 2018
-    """
-    # Remove unnecessary columns from state
-    df_state = df_state.drop(columns=['dem_12', 'rep_12', 'rep_16'])
-    df_state.columns = ['state', 'dem_state']
+    # winning party in 2016
+    df_lower['win_party_16'] = df_lower['2016_win_party']
+    df_upper['win_party_16'] = df_upper['2016_win_party']
 
-    # Remove unnecesary columns from elections
-    elec_cols = ['state', 'district_num', 'dem_elec', 'uncontested']
-    df_elec = df_elec[elec_cols]
+    # Get dem vote share 2018
+    df_lower.loc[df_lower['win_party_16'] == 'R',
+                 '2016_win_margin'] *= -1
+    df_lower['dem_share_16'] = df_lower['2016_win_margin'] / 2 + 0.5
+    df_upper['dem_share_16'] = df_upper['2016_win_margin'] / 2 + 0.5
 
-    # Join statewide voteshare to each prediction
-    df = df.merge(df_state)
-    df['no_incumb_pred'] = df['resid'] + df['dem_state']
+    # Get if race was effectively uncontested
+    df_lower['uncontested_16'] = df_lower['dem_share_16'] - 0.5
+    df_lower['uncontested_16'] = np.abs(df_lower['uncontested_16']) > 0.25
+    df_upper['uncontested_16'] = df_upper['dem_share_16'] - 0.5
+    df_upper['uncontested_16'] = np.abs(df_upper['uncontested_16']) > 0.25
 
-    # Keep relevant columns
-    keep_cols = ['state', 'geoid', 'district_num', 'resid', 'no_incumb_pred']
-    df = df[keep_cols]
+    # Drop unnecessary columns
+    drop_cols = ['2016_win_margin', '2016_win_party']
+    df_lower = df_lower.drop(columns=drop_cols)
+    df_upper = df_upper.drop(columns=drop_cols)
 
-    # Merge with election data
-    df = df.merge(df_elec, how='left')
-
-    # Get difference between election and prediciton
-    df['elec_diff'] = df['dem_elec'] - df['no_incumb_pred']
-
-    # If uncontested let the election be a 30 point swing
-    df.loc[(df['uncontested']) & (df['dem_elec'] > 0.5), 'elec_diff'] = 0.3
-    df.loc[(df['uncontested']) & (df['dem_elec'] <= 0.5), 'elec_diff'] = -0.3
-
-    # Impute average election difference by state
-    df_state = df.groupby('state')['elec_diff'].mean()
-    df_state = pd.DataFrame(df_state).reset_index()
-    df_state.columns = ['state', 'state_diff']
-
-    # Join state difference
-    df = df.merge(df_state)
-
-    # Get the result in 2018
-    df['elec_18'] = df['dem_elec']
-
-    # Keep relevant columns
-    keep_cols = ['geoid', 'resid', 'elec_18', 'elec_diff', 'state_diff']
-    df = df[keep_cols]
+    # Compile into one dataframe
+    df_lower['chamber'] = 'lower'
+    df_upper['chamber'] = 'upper'
+    df = df_lower.append(df_upper)
     return df
 
 
-def foundation_prediction(df, df_econ, df_diff):
-    """Get two party voteshare predictions using foundation model.
-
-    Statewide economist forecast + district presidential residual + incumbency
-    effect
+def add_national_results(df, df_state, df_lower, df_upper):
+    """Add 2016 election results for each state legislative district.
 
     Arguments:
-        df: lower/upper chamber cleaned moneyball data
+        df: historical state legislative election results
 
-        df_econ: current economist forecast
+        df_state: statewide presidential election results
 
-        df_diff: difference between 2018 results and no incumbency foundation
-                 projected"""
-    # Reduce economist data and adjust projected margin to dem voteshare
+        df_lower_resid: lower chamber residuals for national election
+
+        df_upper_resid: upper chamber residuals for national election
+    """
+    # Clean and merge state presidential data
+    df_state = df_state.drop(columns=['dem_12', 'rep_12', 'rep_16'])
+    df_state.columns = ['state', 'dem_state']
+    df = df.merge(df_state)
+
+    # Clean residual data. add chamber type
+    df_lower['chamber'] = 'lower'
+    df_upper['chamber'] = 'upper'
+
+    # Reduce columns
+    keep_cols = ['state', 'district_num', 'resid', 'chamber']
+    df_lower = df_lower[keep_cols]
+    df_upper = df_upper[keep_cols]
+
+    # Create as one dataframe
+    df_resid = df_lower.append(df_upper)
+    df_resid['district_resid'] = df_resid['resid']
+    df_resid = df_resid.drop('resid', axis=1)
+    df_resid['district_num'] = df_resid['district_num'].astype(str)
+    df_resid['district_num'] = df_resid['district_num'].str.zfill(3)
+
+    # Join residuals to election data
+    df = df.merge(df_resid, on=['state', 'district_num', 'chamber'],
+                  how='left')
+
+    # Calculate the district presidential dem voteshare
+    df['dem_pres_16'] = df['dem_state'] + df['district_resid']
+    return df
+
+
+def incumbency_advantage(df):
+    """Add average incumbency advantage over the past two years."""
+    # Get the election difference for each year
+    df['elec_diff_18'] = df['dem_share_18'] - df['dem_pres_16']
+    df['elec_diff_16'] = df['dem_share_16'] - df['dem_pres_16']
+
+    # If uncontested, let the election difference be a 20 point swing
+    df.loc[(df['uncontested_18']) &
+           (df['dem_share_18'] > 0.5), 'elec_diff_18'] = 0.2
+    df.loc[(df['uncontested_18']) &
+           (df['dem_share_18'] <= 0.5), 'elec_diff_18'] = -0.2
+    df.loc[(df['uncontested_16']) &
+           (df['dem_share_16'] > 0.5), 'elec_diff_16'] = 0.2
+    df.loc[(df['uncontested_16']) &
+           (df['dem_share_16'] <= 0.5), 'elec_diff_16'] = -0.2
+
+    # Get the observed incumbency advantage
+    df.loc[df['incumbent_16'].isin(['D', 'R']),
+           'obs_inc_16'] = df['elec_diff_16']
+    df.loc[df['incumbent_18'].isin(['D', 'R']),
+           'obs_inc_18'] = df['elec_diff_18']
+
+    # Get the average incumbency advantage between the two years
+    df['inc_adv'] = np.mean(df[['obs_inc_16', 'obs_inc_18']],
+                            axis=1).fillna(0)
+    return df
+
+
+def clean_incumbency(df):
+    """Clean incumbency data in a dataframe.
+
+    If party is favored and nominee is TBA then assume its
+    and incumbent. Do the same if nominee is False.
+
+    Keep like this because primaries are not finished
+    """
+    df.loc[(df['favored'] == 'D') &
+           (df['nom_D'] == 'TBA'), 'incumbent'] = 'D'
+    df.loc[(df['favored'] == 'D') &
+           (df['nom_D'].astype(str) == 'False'), 'incumbent'] = 'D'
+    df.loc[(df['favored'] == 'R') &
+           (df['nom_R'] == 'TBA'), 'incumbent'] = 'R'
+    df.loc[(df['favored'] == 'R') &
+           (df['nom_R'].astype(str) == 'False'), 'incumbent'] = 'R'
+    df['incumbent_18'] = df['incumbent']
+    return df
+
+
+def foundation_prediction(df, df_econ):
+    """Get final foundations prediction."""
+    # Clean economist margins
     df_econ = df_econ[['state', 'margin']]
     df_econ.columns = ['state', 'state_margin']
-    df_econ['state_voteshare'] = df_econ['state_margin'] / 100 + 0.5
+    df_econ['dem_pres_20'] = df_econ['state_margin'] / 100 + 0.5
+    df_econ = df_econ.drop('state_margin', axis=1)
 
     # Join dataframes
-    df = df.merge(df_econ).merge(df_diff)
+    df = df.merge(df_econ)
 
-    # Adjust state election difference in direction of incumbent
-    df['state_diff'] = df.apply(lambda r: -r['state_diff']
-                                if r['incumbent'] == 'R' else r['state_diff'],
-                                axis=1)
-    df.loc[df['incumbent'].isin(['I', 'False']), 'state_diff'] = 0
-
-    # Calculate incumbency advantage elec_diff
-    df['inc_adv'] = df['elec_diff'].fillna(df['state_diff'])
-
-    # Calculate projected district voteshare w/o incumbency
-    df['found_voteshare'] = df['state_voteshare'] + df['resid']
-
-    # Add incumbency advantage
-    df['is_inc'] = df['incumbent'].apply(lambda x: 1 if x in ['D', 'R'] else 0)
-    df['found_voteshare'] += (df['is_inc'] * df['inc_adv'])
-
-    # Clip voteshare between 0.7 and 0.3
-    df['found_voteshare'] = np.clip(df['found_voteshare'], 0.3, 0.7)
-
-    # Determine if we have results from a past election
-    df['past_election'] = df['elec_diff'].notna()
-
-    # Reduce to geoid and foundations voteshare
-    df = df[['geoid', 'past_election', 'elec_18', 'found_voteshare']]
+    # Get the foundational voteshare and margin
+    df['found_share'] = df['dem_pres_20'] + df['district_resid']
+    df['found_share'] += df['inc_adv']
+    df['found_share'] = np.clip(df['found_share'], 0.3, 0.7)
+    df['found_margin'] = (df['found_share'] - 0.5) * 200
     return df
 
 
