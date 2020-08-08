aeb47ab818f3f9f65c967a10f87e96eb00a2ed8a
jacobwachspress <jacobmw@princeton.edu>
2020-08-08 17:33:50 -0400

    Revert "commit to revert"
    This reverts commit 30b98c8ebfa636884170da2c5646945754983762.

--- redistricting_moneyball.py
+++ redistricting_moneyball.py
@@ -21,10 +21,10 @@ datestring = '_' + str(today.month) + '_' + str(today.day) + '_' + \
                         str(today.year)
 
 # read in input DataFrame
-races_df = pd.read_csv("G:\\Shared drives\\princeton_gerrymandering_project\\Moneyball\\state\\all_input_data.csv")
+races_df = pd.read_csv('data/output/CNalysis/all_input_data.csv')
 
 # read in and merge foundations margin
-pred_path = "G:\\Shared drives\\princeton_gerrymandering_project\\Moneyball\\foundation\\clean\\foundations_predictions_2020_old.csv"
+pred_path = 'data/output/foundation/foundations_predictions_2020.csv'
 founds_df = pd.read_csv(pred_path)
 founds_df['office'] = founds_df['chamber']
 founds_df = founds_df[['state', 'district_num', 'office', 'found_margin']]
@@ -32,7 +32,7 @@ races_df = pd.merge(races_df, founds_df, how='left',
                     on=['state', 'office', 'district_num'])
 
 # read in states to test
-to_test = pd.read_csv("G:\\Shared drives\\princeton_gerrymandering_project\\Moneyball\\state\\states_to_test.csv")
+to_test = pd.read_csv('data/input/parameters/states_and_thresholds.csv')
 
 # merge to get thresholds
 races_df = pd.merge(races_df, to_test, how='left', on=['state', 'office'])
@@ -48,7 +48,7 @@ deg_f_scale = 1 + min(1, 1/4*np.log(1 + days_to_election/20))
 
 # set the correlated error vars
 error_vars = {}
-err_df = pd.read_csv("G:\\Shared drives\\princeton_gerrymandering_project\\Moneyball\\state\\correlated_error.csv")
+err_df = pd.read_csv('data/input/parameters/correlated_error_parameters.csv')
 for _, row in err_df.iterrows():
     sigma = row['sigma']
     deg_f = row['deg_f']
@@ -69,16 +69,16 @@ power_col = 'VOTER_POWER'
 
 # initialize list of bipartisan control probabilities
 bipart_probs = []
+print('finding bipartisan control probs')
+# for each state
 
 states_to_check = ['CT']
-
-# for each state
 for state in states_to_check:
 
     # find probablity of bipartisan control of residistricting
 
     # no blending for NC, all Chaz (redistricting since 2018 messes up founds)
-    if state == 'NC':
+    if state != 'NC':
         bipart_prob = state_voter_powers(races_df, state, error_vars,
                                          race_sigma, race_deg_f, margin_col,
                                          voters_col, threshold_col, tie_col,
@@ -96,7 +96,8 @@ for state in states_to_check:
                                          just_get_prob=True)
 
     bipart_probs.append(bipart_prob)
-print('a'+2)
+    print('a'+2)
+
 # write results to DataFrame
 bipartisan_control_df = pd.DataFrame({'state': states_to_check,
                                       'bipartisan_prob': bipart_probs})
@@ -106,15 +107,12 @@ bipartisan_control_df = pd.DataFrame({'state': states_to_check,
 bipartisan_control_df.to_csv('data/output/voter_power/bipartisan_prob_CT' +\
                              datestring + '.csv', index=False)
 
-bipartisan_control_df.to_csv('data/output/voter_power/bipartisan_prob' +
-                             datestring + '.csv', index=False)
-
 print('win probs done')
 
 results = []
 
 # for each state
-for state in races_df['state'].unique():
+for state in states_to_check:
     print('starting ' + state)
     try:
         # no blending for NC, all Chaz (maps redrawn in 2018)
